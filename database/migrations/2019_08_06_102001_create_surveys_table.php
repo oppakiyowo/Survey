8c3f35ee42c169e5f5948aca9aeb7f85ef347efd
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-06 17:31:44 +0700

    create 3 migration table

---
+++ database/migrations/2019_08_06_102001_create_surveys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal_survey');
+            $table->integer('pindah');
+            $table->integer('meninggal');
+            $table->integer('tidak_diketahui');
+            $table->integer('ganda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+}
---
+++ database/migrations/2019_08_06_102056_create_villages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVillagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('villages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('code');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('villages');
+    }
+}
