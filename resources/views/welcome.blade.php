4b017189fd2e03b52a69d029d8d3265e9b96b8da
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-07 09:43:51 +0700

    update new welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
 <body class="login">
 	<div class="wrapper wrapper-login wrapper-login-full p-0">
 		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-secondary-gradient">
-			<h1 class="title fw-bold text-white mb-3">Join Our Comunity</h1>
+			<h1 class="title fw-bold text-white mb-3">Survey</h1>
 			<p class="subtitle text-white op-7">Ayo bergabung dengan komunitas kami untuk masa depan yang lebih baik</p>
 		</div>
 		<div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
