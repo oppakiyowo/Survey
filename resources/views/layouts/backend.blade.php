3475bce469667ca81b26cc53ed82fc0afe14013e
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-07 11:34:58 +0700

    create layouts

---
+++ resources/views/layouts/backend.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Login</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="{{ asset('assets/img/icon.ico') }}" type="image/x-icon"/>
+
+	<!-- Fonts and icons -->
+	<script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
+	
+
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+	 
+	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/atlantis.css') }}">
+</head>
+
+@yield ('content')
+
+<script src="../assets/js/core/jquery.3.2.1.min.js"></script>
+	<script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+	<script src="../assets/js/core/popper.min.js"></script>
+	<script src="../assets/js/core/bootstrap.min.js"></script>
+	<script src="../assets/js/atlantis.min.js"></script>
+</body>
+</html>
