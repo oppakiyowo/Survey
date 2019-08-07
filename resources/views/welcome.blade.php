39b8a128a48e7e611b2fa25acbcb5bcb40e9945e
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-07 09:11:00 +0700

    add asset sfrom theme atlantis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,121 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Login</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="{{ asset('assets/img/icon.ico') }}" type="image/x-icon"/>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<!-- Fonts and icons -->
+	<script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
+	
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+	<script>
+		WebFont.load({
+			google: {"families":["Lato:300,400,700,900"]},
+			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+	 
+	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/css/atlantis.css') }}">
 
-            .full-height {
-                height: 100vh;
-            }
+	
+</head>
+<body class="login">
+	<div class="wrapper wrapper-login wrapper-login-full p-0">
+		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-secondary-gradient">
+			<h1 class="title fw-bold text-white mb-3">Join Our Comunity</h1>
+			<p class="subtitle text-white op-7">Ayo bergabung dengan komunitas kami untuk masa depan yang lebih baik</p>
+		</div>
+		<div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
+			<div class="container container-login container-transparent animated fadeIn">
+				<h3 class="text-center">Sign In To Admin</h3>
+				<div class="login-form">
+					<div class="form-group">
+						<label for="username" class="placeholder"><b>Username</b></label>
+						<input id="username" name="username" type="text" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="password" class="placeholder"><b>Password</b></label>
+						<a href="#" class="link float-right">Forget Password ?</a>
+						<div class="position-relative">
+							<input id="password" name="password" type="password" class="form-control" required>
+							<div class="show-password">
+								<i class="icon-eye"></i>
+							</div>
+						</div>
+					</div>
+					<div class="form-group form-action-d-flex mb-3">
+						<div class="custom-control custom-checkbox">
+							<input type="checkbox" class="custom-control-input" id="rememberme">
+							<label class="custom-control-label m-0" for="rememberme">Remember Me</label>
+						</div>
+						<a href="#" class="btn btn-secondary col-md-5 float-right mt-3 mt-sm-0 fw-bold">Sign In</a>
+					</div>
+					<div class="login-account">
+						<span class="msg">Don't have an account yet ?</span>
+						<a href="#" id="show-signup" class="link">Sign Up</a>
+					</div>
+				</div>
+			</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+			<div class="container container-signup container-transparent animated fadeIn">
+				<h3 class="text-center">Sign Up</h3>
+				<div class="login-form">
+					<div class="form-group">
+						<label for="fullname" class="placeholder"><b>Fullname</b></label>
+						<input  id="fullname" name="fullname" type="text" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="email" class="placeholder"><b>Email</b></label>
+						<input  id="email" name="email" type="email" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="passwordsignin" class="placeholder"><b>Password</b></label>
+						<div class="position-relative">
+							<input  id="passwordsignin" name="passwordsignin" type="password" class="form-control" required>
+							<div class="show-password">
+								<i class="icon-eye"></i>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="confirmpassword" class="placeholder"><b>Confirm Password</b></label>
+						<div class="position-relative">
+							<input  id="confirmpassword" name="confirmpassword" type="password" class="form-control" required>
+							<div class="show-password">
+								<i class="icon-eye"></i>
+							</div>
+						</div>
+					</div>
+					<div class="row form-sub m-0">
+						<div class="custom-control custom-checkbox">
+							<input type="checkbox" class="custom-control-input" name="agree" id="agree">
+							<label class="custom-control-label" for="agree">I Agree the terms and conditions.</label>
+						</div>
+					</div>
+					<div class="row form-action">
+						<div class="col-md-6">
+							<a href="#" id="show-signin" class="btn btn-danger btn-link w-100 fw-bold">Cancel</a>
+						</div>
+						<div class="col-md-6">
+							<a href="#" class="btn btn-secondary w-100 fw-bold">Sign Up</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+	<script src="../assets/js/core/jquery.3.2.1.min.js"></script>
+	<script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+	<script src="../assets/js/core/popper.min.js"></script>
+	<script src="../assets/js/core/bootstrap.min.js"></script>
+	<script src="../assets/js/atlantis.min.js"></script>
+</body>
 </html>
