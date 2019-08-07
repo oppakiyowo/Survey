c85e55d94cccc85c653f9ce6112976722e18e2ba
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-07 08:16:22 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('surveyors', 'SurveyorController');
+Route::resource('Villages', 'VillageController');
+Route::resource('Surveys', 'SurveyController'); 
