de7f2a0d21c79057478459d4265903375903d80e
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-12 10:48:47 +0700

    add toastr package

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,11 +19,14 @@
 			}
 		});
 	</script>
-	 
+	  <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
+	  <link  href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+	  <script src="{{ asset('js/toastr.min.js') }}"></script>
 	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('assets/css/atlantis.css') }}">
 </head>
 <body class="login">
+	@include ('partial.message')
 	<div class="wrapper wrapper-login wrapper-login-full p-0">
 		<div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center bg-secondary-gradient">
 			<h1 class="title fw-bold text-white mb-3">Survey</h1>
@@ -48,7 +51,7 @@
 					<div class="form-group">
 							<label for="password">{{ __('Password') }}</label>
 						@if (Route::has('password.request'))
-						<a class="link float-right "href="{{ route('password.request') }}">
+						<a class="link float-right link" href="#" id="show-signup" >
 							{{ __('Forgot Your Password?') }}
 						</a>
 					@endif
@@ -77,40 +80,25 @@
 				
 					<div class="login-account">
 						<span class="msg">Don't have an account yet ?</span>
-						<a href="/register" id="show-signup" class="link">Sign Up</a>
+						<a href="/register" class="link">Sign Up</a>
 					</div>
 
 				</div>
 			</div>
 		</form>
 			<div class="container container-signup container-transparent animated fadeIn">
-				<h3 class="text-center">Sign Up</h3>
+				<h3 class="text-center">Reset Password</h3>
 				<div class="login-form">
+					<form method="POST" action="{{ route('password.email') }}">
+                        @csrf
 					<div class="form-group">
-						<label for="fullname" class="placeholder"><b>Fullname</b></label>
-						<input  id="fullname" name="fullname" type="text" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label for="email" class="placeholder"><b>Email</b></label>
-						<input  id="email" name="email" type="email" class="form-control" required>
-					</div>
-					<div class="form-group">
-						<label for="passwordsignin" class="placeholder"><b>Password</b></label>
-						<div class="position-relative">
-							<input  id="passwordsignin" name="passwordsignin" type="password" class="form-control" required>
-							<div class="show-password">
-								<i class="icon-eye"></i>
-							</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="confirmpassword" class="placeholder"><b>Confirm Password</b></label>
-						<div class="position-relative">
-							<input  id="confirmpassword" name="confirmpassword" type="password" class="form-control" required>
-							<div class="show-password">
-								<i class="icon-eye"></i>
-							</div>
-						</div>
+						<label for="email" class="placeholder"><b>Email Address</b></label>
+						<input  id="email" name="email" type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email" autofocus required>
+							@error('email')
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+							@enderror
 					</div>
 					<div class="row form-sub m-0">
 						<div class="custom-control custom-checkbox">
@@ -123,8 +111,10 @@
 							<a href="#" id="show-signin" class="btn btn-danger btn-link w-100 fw-bold">Cancel</a>
 						</div>
 						<div class="col-md-6">
-							<a href="#" class="btn btn-secondary w-100 fw-bold">Sign Up</a>
+							<button type="submit" class="btn btn-secondary w-100 fw-bold">Reset Password</button>
 						</div>
+						
+					</form>
 					</div>
 				</div>
 			</div>
