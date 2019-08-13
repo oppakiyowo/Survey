9ca5a4104b5cec7b12460c620dce19f86f6d9ccb
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-13 14:42:05 +0700

    edit vilage model

--- app/Village.php
+++ app/Village.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Village extends Model
 {
-    //
+    protected $fillable = [
+        'name', 
+        'code',
+        'total_penduduk'
+    
+    ];  
+
+     public function surveys()
+        {
+            return $this->hasMany(Survey::class);
+        }
 }
