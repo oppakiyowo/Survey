1675a2e5a752651f6b03a1e8e2d0cf22d160e29c
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-14 13:51:11 +0700

    fix header image

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -42,7 +42,7 @@
 			<div class="logo-header" data-background-color="blue">
 				
 				<a href="/home" class="logo">
-					<img src="../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">
+					<img src="{{ asset('assets/img/logo.svg') }}" alt="navbar brand" class="navbar-brand">
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
