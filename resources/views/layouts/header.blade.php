c273ea1c850e509d518c6dbb442f07989dc82ea2
edo <edolorenza1@gmail.com>
2019-08-17 20:02:40 +0700

    delete demo ccs

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,9 +31,6 @@
 	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('assets/css/atlantis.min.css') }}">
 
-	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
-
 </head>
 <body>
 	<div class="wrapper">
