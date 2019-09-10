18db5bb5b2c1a265879a6ac2fca2d54147ccc80a
edo <edolrnz@gmail.com>
2019-09-10 11:46:52 +0700

    add user change password function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Survey;
+use Auth;
 use App\Http\Requests\user\CreateUsersRequest;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -94,12 +100,48 @@ class UserController extends Controller
             session()->flash('error', 'User ini Tidak bisa di Hapus');
             return redirect()->route('users.index');
         }
-        
-        
+
+
         $user->delete();
 
         session()->flash('success', 'User berhasil di hapus');
         return redirect(route('users.index'));
     }
 
-}
+    public function ChangePassword()
+    {
+
+        return view('users.changepassword');
+    }
+
+    public function UpdatePassword(Request $request){
+
+
+        $validatedData = $request->validate([
+            'new-password' => 'required|min:6',
+
+        ]);
+
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+        return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+        if(!(strcmp($request->get('new-password'), $request->get('new-password-confirm'))) == 0){
+            //New password and confirm password are not same
+            return redirect()->back()->with("error","New Password should be same as your confirmed password. Please retype new password.");
+         }
+         //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+
+        return redirect(route('users.index'))->with("success","Password changed successfully !");
+
+
+        }
+
+ }
